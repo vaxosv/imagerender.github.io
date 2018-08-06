811966c499f03257dce83ad8a8874c326469740e
vakhtangi <vaxo.sv@gmail.com>
2018-08-07 02:38:15 +0400

    image

--- bubble.js
+++ bubble.js
@@ -2,15 +2,15 @@ function Bubble(x, y, c) {
     this.x = x;
     this.y = y;
     this.lifespan = 255;
-    this.diameter = 5;
+    this.diameter = 4;
     this.display = function () {
         fill(c, this.lifespan);
         noStroke()
         ellipse(this.x, this.y, this.diameter, this.diameter);
     };
     this.update = function () {
-        this.x = this.x + random(-2, 2);
-        this.y = this.y + random(-2, 2);
+        this.x = this.x + random(-1, 1);
+        this.y = this.y + random(-1, 1);
         this.lifespan -= 5;
     };
     this.isFinished = function () {
--- js.js
+++ js.js
@@ -1,34 +1,38 @@
 var video;
 var button;
 var arr = [];
-var bubbles= []
+var bubbles = []
+
 function preload() {
-    img = loadImage("n.jpg")
+    img = loadImage("./n.jpg")
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    // background(51)
+    createCanvas(img.width, img.height)
     frameRate(60);
 }
 
 function draw() {
-    x = random(width)
-    y = random(height)
+    // for (let g = 0; g <= 100; g++) {
+        x = random(width)
+        y = random(height)
 
-    c = img.get(x, y)
-    fill(c);
-    noStroke()
-    // ellipse(x, y, 12, 12)
+        c = img.get(x, y)
+        fill(c);
+        noStroke()
+        // ellipse(x, y, 12, 12)
 
-    var b = new Bubble(x, y, c);
-    bubbles.push(b);
-    // background(51);
-    for (var i = bubbles.length - 1; i >= 0; i--) {
-        bubbles[i].update();
-        bubbles[i].display();
-        if (bubbles[i].isFinished()) {
-            bubbles.splice(i, 1);
+        var b = new Bubble(x, y, c);
+        bubbles.push(b);
+        // background(51);
+        for (var i = bubbles.length - 1; i >= 0; i--) {
+            bubbles[i].update();
+            bubbles[i].display();
+            if (bubbles[i].isFinished()) {
+                bubbles.splice(i, 1);
+            }
         }
-    }
-    
+    // }
+
 }
